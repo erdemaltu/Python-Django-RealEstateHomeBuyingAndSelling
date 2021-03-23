00e0152525ae803a8e1e90e23242a6c3487b85fb
erdemaltu <e-altug@hotmail.com>
2021-03-23 18:12:09 +0300

    model-category-and-product-relation

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Category
+from home.models import Category, Home
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
+class HomeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'price', 'status']
+    list_filter = ['status']
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Home, HomeAdmin)
--- home/models.py
+++ home/models.py
@@ -18,3 +18,54 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+class Home(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with Category Table
+    title = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    price = models.FloatField()
+    square_meters = models.PositiveIntegerField()
+    NUMBER_OF_ROOMS = (
+        ('Default', 'Belirtilmemiş'),
+        ('1', '1+1'),
+        ('2', '2+1'),
+        ('3', '3+1'),
+        ('4', '4+1'),
+    )
+    number_of_rooms = models.CharField(max_length=30, choices=NUMBER_OF_ROOMS, default='Default')
+    building_age = models.PositiveIntegerField()
+    floor_location = models.IntegerField()
+    number_of_floors = models.PositiveIntegerField()
+    FURNISHED = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    furnished = models.CharField(max_length=30, choices=FURNISHED, default='False')
+    USING_STATUS = (
+        ('Idle', 'Boşta'),
+        ('OnRent', 'Kirada'),
+    )
+    using_status = models.CharField(max_length=30, choices=USING_STATUS, default='Idle')
+    dues = models.PositiveIntegerField()
+    FROM_WHO = (
+        ('FromTheOwner', 'Sahibinden'),
+        ('FromRealEstateAgent', 'Emlakçıdan'),
+    )
+    from_who = models.CharField(max_length=30, choices=FROM_WHO, default='FromTheOwner')
+    SWAP = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    swap = models.CharField(max_length=30, choices=SWAP, default='False')
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
